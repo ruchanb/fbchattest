d302d9506d525c86a56084043778331bc7399e30
Ruchan <myruchan@gmail.com>
2021-02-20 23:09:22 +0545

    .

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php include_once("fbchat.html"); ?>
+<?php /*include_once("fbchat.html");*/
+echo 'List of headers <br>';
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $chunks = explode('_', $key);
+        $header = '';
+        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
+            $header .= ucfirst(strtolower($chunks[$i])).'-';
+        }
+        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
+        echo $header.'<br>';
+    }
+}
+?>
